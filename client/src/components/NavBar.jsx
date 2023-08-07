34ef48e5d494ce13e698884acfd2f4a6429c3207
Jeremy Cox <jeremy@theoutlet.org>
2023-08-07 18:06:30 -0400

    update navbar and introduce tailwindcss custom colors

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -16,19 +16,67 @@ const NavBar = () => {
     setUser(JSON.parse(localStorage.getItem('profile')))
   }, [location])
   return (
-    <nav className="nav">
-      <Link to="/">
-        <h3>OMNI</h3>
-      </Link>
-      <div>
-        <NavLink to="/">Home </NavLink>
-        <NavLink to="/categories">Categories</NavLink>
-        {!user ? (
-          <NavLink to="/auth">Signup/Login</NavLink>
-        ) : (
-          <NavLink to="/new">New Article</NavLink>
-        )}
-        {!user ? null : <button onClick={logout}>Logout</button>}
+    <nav class="bg-white dark:bg-secondary fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
+      <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <a href="/" class="flex items-center">
+          <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+            OMNI
+          </span>
+        </a>
+        <div class="flex md:order-2">
+          {!user ? (
+            <NavLink to="/auth">
+              <button
+                type="button"
+                class="text-white bg-primary hover:bg-teritiary focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-primary"
+              >
+                Signup/Login
+              </button>
+            </NavLink>
+          ) : (
+            <button
+              type="button"
+              onClick={logout}
+              class="text-white bg-primary hover:bg-teritiary focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-primary"
+            >
+              Logout
+            </button>
+          )}
+        </div>
+        <div
+          class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+          id="navbar-sticky"
+        >
+          <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-secondary md:dark:bg-secondary dark:border-gray-700">
+            <li>
+              <a
+                href="/"
+                class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-primary md:p-0 md:dark:hover:text-primary dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                aria-current="page"
+              >
+                Home
+              </a>
+            </li>
+            <li>
+              <a
+                href="/categories"
+                class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-primary md:p-0 md:dark:hover:text-primary dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+              >
+                Categories
+              </a>
+            </li>
+            {user ? (
+              <li>
+                <a
+                  href="/new"
+                  class="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-primary md:p-0 md:dark:hover:text-primary dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                >
+                  New Article
+                </a>
+              </li>
+            ) : null}
+          </ul>
+        </div>
       </div>
     </nav>
   )
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        secondary: '#8AC6D0',
+        primary: '#C65B7C',
+        teritiary: '#5B3758'
+      }
+    }
   },
   plugins: []
 }
