a59d18948a5945f743d93b2df8938b4b1fe5eadf
Jeremy Cox <jeremy@theoutlet.org>
2023-08-09 16:41:24 -0400

    add onfocus and onblur to searchbar

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -2,7 +2,12 @@ function Search() {
   return (
     <div className="search">
       <form>
-        <input placeholder="what are you looking for?" type="text" />
+        <input
+          placeholder="what are you looking for?"
+          type="text"
+          onFocus={(e) => (e.target.placeholder = '')}
+          onBlur={(e) => (e.target.placeholder = 'what are you looking for?')}
+        />
       </form>
     </div>
   )
