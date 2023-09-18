160879997a6ffb9b2735c7bd94c5641f46e52840
Jeremy Cox <jeremy@theoutlet.org>
2023-09-18 17:52:21 -0400

    update start script

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ const app = express()
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
-app.use(bodyParser.json({ limit: '300kb' }))
+app.use(bodyParser.json({ limit: '10mb' }))
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
