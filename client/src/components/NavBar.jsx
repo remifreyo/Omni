d29a6f3a8f9af6b7036c3afff3cd0b29b026266d
Jeremy Cox <jeremy@theoutlet.org>
2023-09-18 18:18:04 -0400

    update links

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -57,23 +57,21 @@ const NavBar = () => {
                 Home
               </a>
             </li>
-            <li>
-              <a
-                href="/categories"
-                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-primary md:p-0 md:dark:hover:text-primary dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-              >
-                Categories
-              </a>
-            </li>
-            {user ? (
+            <NavLink to="/categories">
               <li>
-                <a
-                  href="/new"
-                  className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-primary md:p-0 md:dark:hover:text-primary dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                >
-                  New Article
+                <a className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-primary md:p-0 md:dark:hover:text-primary dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+                  Categories
                 </a>
               </li>
+            </NavLink>
+            {user ? (
+              <NavLink to="/new">
+                <li>
+                  <a className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-primary md:p-0 md:dark:hover:text-primary dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+                    New Article
+                  </a>
+                </li>
+              </NavLink>
             ) : null}
           </ul>
         </div>
